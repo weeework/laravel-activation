ea1d99664187821330552b41221e393b396b20a3
Hxor <4kiddie04@gmail.com>
2017-11-27 20:04:06 +0700

    prevent login access through password reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -36,4 +36,40 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = bcrypt($password);
+
+        $user->setRememberToken(str_random(60));
+
+        $user->save();
+
+        if ($user->active) {
+            $this->guard()->login($user);
+        }
+
+        // event(new PasswordReset($user));
+
+        
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse($response)
+    {
+        return redirect()->route('login')
+            ->withSuccess('Your password has been reset successfully.');
+    }
 }
