9d3f1e42f75d8f47cb1a9545a82ac5a57b75ea84
Hxor <4kiddie04@gmail.com>
2017-11-27 17:23:09 +0700

    add activation columns and protect login with active status

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,36 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => [ //Make an custom array
+                'required','string',
+                Rule::exists('users')->where(function ($query) { //create closure with query builder to check the users
+                    $query->where('active', true); //where active column is true
+                })
+            ],
+            'password' => 'required|string',
+        ], $this->validationErrors());
+    }
+
+    /**
+     * Get the validation errors for login.
+     *
+     * @return array
+     */
+    public function validationErrors()
+    {
+        return [
+            $this->username() . '.exists' => 'The selected email is invalid or you need to activate your account.'
+        ];
     }
 }
